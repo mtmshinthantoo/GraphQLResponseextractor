a169c6897a5690b7e3a61b87e81d4b0316c48190
Shin Thant Oo <mtm.shinthantoo@gmail.com>
2026-05-27 14:19:11 +0630

    Refector for FlattenHandler

--- src/main/java/com/testshin/extractor/handler/ValueHandler.java
+++ src/main/java/com/testshin/extractor/handler/ValueHandler.java
@@ -1,4 +1,22 @@
 package com.testshin.extractor.handler;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.testshin.extractor.util.JsonUtils;
+
+import java.util.List;
+import java.util.Map;
+
 public class ValueHandler {
+
+    public static void applyValue(
+            List<Map<String, String>> rows,
+            String column,
+            JsonNode node) {
+
+        String value = JsonUtils.safeText(node);
+
+        for (Map<String, String> row : rows) {
+            row.put(column, value);
+        }
+    }
 }
